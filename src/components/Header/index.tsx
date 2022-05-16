91ff5cecb7292b483b9dfee65da93eac16cc301d
Camily Cruz <camilycruz2003@gmail.com>
2022-05-16 01:08:00 -0300

    alteracao

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import logo from "../public/assets/logo.png"
+
+export const Header = ()=>{
+    return(
+        <div className="header">
+            <img src={logo} alt="" />
+            {/* <></> construtores*/}
+        </div>
+    )
+}
