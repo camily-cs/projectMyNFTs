171000c056f0351e119ce932073dce368491c1bf
Camily Cruz <camilycruz2003@gmail.com>
2022-05-16 10:44:29 -0300

    Finalizacao do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
 import logo from "../public/assets/logo.png"
 
+
 export const Header = ()=>{
     return(
         <div className="header">
-            <img src={logo} alt="" />
-            {/* <></> construtores*/}
+            <img src="./assets/img/logo.png" alt="logo" className="logo"/>
+            
+            
+            <div className="menu">
+                <h3>Home</h3>
+                <h3>Sobre</h3>
+                <h3>Roadmap</h3>
+                <h3>Team</h3>
+                <h3>NFTs</h3>
+                <h3>On Blog</h3>
+            </div>
+            
         </div>
     )
 }
